afe5589c1d7a4accd16119b22828de4b11f0b081
Jonathan Hammond <jonathan.hammond@isitetv.com>
2023-08-10 23:11:38 +0100

    Set-Up-Relationships

--- PokemonReviewApp/Models/Review.cs
+++ PokemonReviewApp/Models/Review.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Text { get; set; } = string.Empty;
+        public Reviewer Reviewer { get; set; }
+        public Pokemon Pokemon { get; set; }
     }
 }
--- PokemonReviewApp/Models/Reviewer.cs
+++ PokemonReviewApp/Models/Reviewer.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
+        public ICollection<Review> Reviews { get; set; }
     }
 }
