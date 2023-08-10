26a4684110327f0a1be755924318016ea3c31ebf
Jonathan Hammond <jonathan.hammond@isitetv.com>
2023-08-10 16:11:21 +0100

    Create Models

---
+++ PokemonReviewApp/Models/Review.cs
@@ -0,0 +1,9 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Text { get; set; } = string.Empty;
+    }
+}
