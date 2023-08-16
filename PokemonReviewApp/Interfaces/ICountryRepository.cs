b084212c4a40e085120733523a4ef72debc4451d
Jonathan Hammond <jonathan.hammond@isitetv.com>
2023-08-16 14:24:07 +0100

    Implement-repositories

---
+++ PokemonReviewApp/Interfaces/ICountryRepository.cs
@@ -0,0 +1,14 @@
+﻿using PokemonReviewApp.Models;
+using System.Collections;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface ICountryRepository
+    {
+        ICollection<Country> GetCountries();
+        ICollection<Owner> GetOwnersFromACountry(int countryId);
+        Country GetCountry(int id);
+        Country GetCountryByOwner(int ownerId);
+        bool CountryExists(int id);
+    }
+}
---
+++ PokemonReviewApp/Repository/CountryRepository.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class CountryRepository : ICountryRepository
+    {
+        private DataContext _context;
+        public CountryRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public bool CountryExists(int id)
+        {
+            return _context.Countries.Any(c =>  c.Id == id);
+        }
+
+        public Country GetCountry(int id)
+        {
+            return _context.Countries.Where(e => e.Id == id).FirstOrDefault();
+        }
+        
+        public ICollection<Country> GetCountries()
+        {
+            return _context.Countries.ToList();
+        }
+
+        public Country GetCountryByOwner(int ownerId)
+        {
+            return _context.Owners.Where(e => e.Id == ownerId).Select(c => c.Country).FirstOrDefault();
+        }
+
+        public ICollection<Owner> GetOwnersFromACountry(int countryId)
+        {
+            return _context.Owners.Where(c => c.Country.Id == countryId).ToList();
+        }
+    }
+}
